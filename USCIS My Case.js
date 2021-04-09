7856ad2508a8cff8d2b0cbfe5efc94b627cb216f
Siddharth <78608818+iamanonym0us@users.noreply.github.com>
2021-04-09 11:19:43 -0400

    Update USCIS My Case.js

--- USCIS My Case.js
+++ USCIS My Case.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-brown; icon-glyph: magic;
-const receipt = args.widgetParameter?args.widgetParameter:'YSC2190052500'
+const receipt = args.widgetParameter?args.widgetParameter:'YSC21900525'
 
 const url = `https://egov.uscis.gov/casestatus/landing.do`
 main()
